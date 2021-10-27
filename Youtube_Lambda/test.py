04fd11f1bf9d626765d217c44c1314025265a177
nr97819 <nr97819@naver.com>
2021-10-28 06:46:30 +0900

    lambda 실습

---
+++ Youtube_Lambda/test.py
@@ -0,0 +1,18 @@
+import json
+import urllib.request
+from bs4 import BeautifulSoup
+
+url="https://www.google.com"
+soup=BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser", from_encoding='euc-kr')
+tags=soup.find_all('a') # a 태그를 모두 찾기
+response_list=list()
+for item in tags:
+    response_list.append(item.get_text())
+print(response_list)
+
+# def lambda_handler(event, context):
+#     # TODO implement
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps('Hello Lambda!')
+#     }
